5e979370b5c3ca6d0eeda83789aa863d2be69f11
Vitaliy Vasilenko <me@vitalvas.com>
2022-06-09 01:06:29 +0300

    dns: move to runner

--- app/dns/main.go
+++ app/dns/main.go
@@ -1,45 +1,10 @@
 package main
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
 	"github.com/vitalvas/cloudrouter/lib/dns"
-	"github.com/vitalvas/cloudrouter/lib/logger"
+	"github.com/vitalvas/cloudrouter/lib/runner"
 )
 
-var log = logger.NewConsole()
-
 func main() {
-	srv := dns.NewServer()
-
-	defer srv.Shutdown()
-
-	if err := srv.Apply(); err != nil {
-		log.Fatal("error start server: %w", err)
-	}
-
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGUSR1, syscall.SIGINT)
-
-	go func() {
-		for {
-			time.Sleep(time.Hour)
-			ch <- syscall.SIGUSR1
-		}
-	}()
-
-	for sig := range ch {
-		if sig == syscall.SIGUSR1 {
-			if err := srv.Apply(); err != nil {
-				log.Printf("error update listener: %v", err)
-			}
-		} else if sig == syscall.SIGINT {
-			break
-		}
-	}
-
-	log.Println("shutdown")
+	runner.Execute(dns.NewServer())
 }
